3f3fb04cf5ecd5a80a242d70e93b3645c4f4b19f
Jarikkomarik <ufland.ufland@gmail.com>
2024-05-07 21:56:16 +0200

    init commit.

---
+++ QueryService/src/main/java/jarikkomarik/QueryService.java
@@ -0,0 +1,20 @@
+package jarikkomarik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class QueryService {
+    public static void main(String[] args) {
+        SpringApplication.run(QueryService.class, args);
+    }
+
+    @GetMapping("hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok().body("Hello from QueryService");
+    }
+}
